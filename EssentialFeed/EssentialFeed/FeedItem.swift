29cfe495f65d6a321eb1e188139da91db1f172e3
danisot <jfsotelo96@gmail.com>
2023-02-18 12:47:57 +0100

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by José Daniel Fernández Sotelo on 18/2/23.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
