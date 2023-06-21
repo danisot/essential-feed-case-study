3f6ac6fc67ab71d11f13ea56bee2798cc00ee14c
danisot <jfsotelo96@gmail.com>
2023-06-21 12:19:18 +0200

    Save command does not request cache insertion on deletion error

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -23,9 +23,14 @@ class LocalFeedLoader {
 
 class FeedStore {
 	var deleteCacheCallCount = 0
+	var insertCallCount = 0
 
 	func deleteCachedFeed() {
 		deleteCacheCallCount += 1
+	}
+
+	func completeDeletion(with error: Error, at index: Int = 0) {
+
 	}
 }
 
@@ -46,6 +51,18 @@ final class CacheFeedUseCaseTests: XCTestCase {
 		XCTAssertEqual(store.deleteCacheCallCount, 1)
 	}
 
+	func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+		let deletionError = anyNSError()
+
+		sut.save(items)
+		store.completeDeletion(with: deletionError)
+
+
+		XCTAssertEqual(store.insertCallCount, 0)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
@@ -62,5 +79,9 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
 	private func anyURL() -> URL {
 		URL(string: "http://any-url.com")!
+	}
+
+	private func anyNSError() -> NSError {
+		NSError(domain: "anyError", code: 1)
 	}
 }
