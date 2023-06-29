08f0e4feed83428289f7119e69615373516b8f88
danisot <jfsotelo96@gmail.com>
2023-06-29 13:14:53 +0200

    Separate Date extension helpers into distinct contexts to clarify their scope (one is a cache-policy specific DSL and the other is just a reusable DSL helper)

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -26,16 +26,19 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 }
 
 extension Date {
-
+    
     private var feedCacheMaxAgeInDays: Int { 7 }
-
+    
     func minusFeedCacheMaxAge() -> Date {
         self.adding(days: -feedCacheMaxAgeInDays)
     }
-
-    func adding(days: Int) -> Date {
+    
+    private func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
+}
+
+extension Date {
 
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
