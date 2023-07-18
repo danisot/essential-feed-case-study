0ddeb4f473569e8b9b37f407037b5a2e3551ab0c
danisot <jfsotelo96@gmail.com>
2023-07-18 15:49:45 +0200

    Move Viewmodels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by José Daniel Fernández Sotelo on 18/7/23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by José Daniel Fernández Sotelo on 18/7/23.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
