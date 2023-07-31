1474043b87b38fd30999fad4b0491a9b2c5bf710
danisot <jfsotelo96@gmail.com>
2023-07-31 15:09:53 +0200

    Move error view configuration to storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -19,7 +19,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
     var delegate: FeedViewControllerDelegate?
 
-    public let errorView = ErrorView()
+    @IBOutlet private(set) public var errorView: ErrorView?
 
     var tableModel: [FeedImageCellController] = [] {
         didSet {
@@ -38,7 +38,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     func display(_ viewModel: FeedErrorViewModel) {
-        errorView.message = viewModel.message
+        errorView?.message = viewModel.message
     }
 
     @IBAction private func refresh() {
