dff5ae11f7e7837667cdd4d6293a8f0ffbe1e9b2
danisot <jfsotelo96@gmail.com>
2023-07-31 12:11:04 +0200

    Dispatch background feed completion to main thread before updating the UI since UIKit is not thread safe

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -17,7 +17,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
     var tableModel: [FeedImageCellController] = [] {
         didSet {
-            tableView.reloadData()
+            if Thread.isMainThread {
+                tableView.reloadData()
+            } else {
+                DispatchQueue.main.async { [weak self] in
+                    self?.tableView.reloadData()
+                }
+            }
         }
     }
 
@@ -28,6 +34,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     func display(_ viewModel: FeedLoadingViewModel) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }
+        }
+
         viewModel.isLoading ? refreshControl?.beginRefreshing() : refreshControl?.endRefreshing()
     }
 
