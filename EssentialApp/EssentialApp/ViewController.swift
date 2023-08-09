9e7ed6ac4ef7f002cc046ad9a420cee49c677f2e
danisot <jfsotelo96@gmail.com>
2023-08-09 09:15:26 +0200

    Add EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by José Daniel Fernández Sotelo on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
