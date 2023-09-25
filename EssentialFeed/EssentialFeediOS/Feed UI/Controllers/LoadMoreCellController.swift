be156d0f7b6e0d84a7b15373fd5dd0c4c3996380
danisot <jfsotelo96@gmail.com>
2023-09-25 18:06:20 +0200

    Automatically load more items on scroll after an error

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -12,6 +12,8 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableVie
     private let cell = LoadMoreCell()
     private let callback: () -> Void
 
+    private var offsetObserver: NSKeyValueObservation?
+
     public init(callback: @escaping () -> Void) {
         self.callback = callback
     }
@@ -31,6 +33,16 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableVie
 
     public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
         reloadIfNeeded()
+
+        offsetObserver = tableView.observe(\.contentOffset, options: .new) { [weak self] (tableView, _) in
+            guard tableView.isDragging else { return }
+
+            self?.reloadIfNeeded()
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        offsetObserver = nil
     }
 
     private func reloadIfNeeded() {
