99d510d7e8930eefdb50076e6b7f1676add498c5
danisot <jfsotelo96@gmail.com>
2023-09-26 17:42:05 +0200

    Reuse existing cell controllers to avoid unnecessary reallocation of resources

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -16,19 +16,27 @@ final class FeedViewAdapter: ResourceView {
     private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
     private let selection: (FeedImage) -> Void
+    private let currentFeed: [FeedImage: CellController]
 
     init(
+        currentFeed: [FeedImage: CellController] = [:],
         controller: ListViewController,
         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
         selection: @escaping (FeedImage) -> Void
     ) {
+        self.currentFeed = currentFeed
         self.controller = controller
         self.imageLoader = imageLoader
         self.selection = selection
     }
 
     func display(_ viewModel: Paginated<FeedImage>) {
+        guard let controller else { return }
+
+        var currentFeed = self.currentFeed
         let feed = viewModel.items.map { model in
+            if let controller = currentFeed[model] { return controller }
+
             let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
                 imageLoader(model.url)
             })
@@ -51,11 +59,14 @@ final class FeedViewAdapter: ResourceView {
                     return image
                 })
 
-            return CellController(id: model, view)
+            let controller = CellController(id: model, view)
+            currentFeed[model] = controller
+
+            return controller
         }
 
         guard let loadMorePublisher = viewModel.loadMorePublisher else {
-            controller?.display(feed)
+            controller.display(feed)
             return
         }
 
@@ -64,14 +75,18 @@ final class FeedViewAdapter: ResourceView {
         let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
 
         loadMoreAdapter.presenter = LoadResourcePresenter(
-            resourceView: self,
+            resourceView: FeedViewAdapter(
+                currentFeed: currentFeed,
+                controller: controller,
+                imageLoader: imageLoader,
+                selection: selection),
             errorView: WeakRefVirtualProxy(loadMore),
             loadingView: WeakRefVirtualProxy(loadMore),
             mapper: { $0 })
 
         let loadMoreSection = [CellController(id: UUID(), loadMore)]
 
-        controller?.display(feed, loadMoreSection)
+        controller.display(feed, loadMoreSection)
     }
 }
 
